c778eba42eeb51163fb150ec6e6068abe8594b90
bess04111-star <xiebess04111@gmail.com>
2026-09-16 16:46:28 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+window.GOOGLE_API_URL = "https://script.google.com/macros/s/AKfycbxrCPwQllExDWZuMIzpWA-bgklDKimhwVacG0W2d9BgmODfYe5Qi2hHIlFuK-OoMEx5/exec";
+window.API_SECRET_TOKEN = "KPI_MASTER_SECURE_TOKEN_2026";
